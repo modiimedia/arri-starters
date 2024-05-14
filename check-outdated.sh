2047b7fa8cde4d24bb290e73e7d1256c66e77aad
joshmossas <josh@modiimedia.com>
2024-05-14 11:09:35 -0500

    add outdated script + add prettier format script

---
+++ check-outdated.sh
@@ -0,0 +1,3 @@
+(cd app-starter-ts ; echo "\nChecking outdated in \"app-starter-ts\"" ; pnpm outdated)
+(cd app-starter-ts-with-eslint ; echo "\nChecking outdated in \"app-starter-ts-with-eslint\"" ; pnpm outdated)
+(cd generator-starter ; echo "\nChecking outdated in \"generator-starter\"" ; pnpm outdated)
--- update-arri.sh
+++ update-arri.sh
@@ -1,2 +1,3 @@
-(cd app-starter-ts ; pnpm i arri@latest @arrirpc/server@latest @arrirpc/schema@latest)
-(cd generator-starter ; pnpm i @arrirpc/codegen-utils@latest)
+(cd app-starter-ts ; echo "\nUpdating \"app-starter-ts\"" ; pnpm i arri@latest @arrirpc/server@latest @arrirpc/schema@latest)
+(cd app-starter-ts-with-eslint ; echo "\nUpdating \"app-starter-ts-with-eslint\"" ; pnpm i arri@latest @arrirpc/server@latest @arrirpc/schema@latest)
+(cd generator-starter ; echo "\nUpdating \"generator-starter\"" ; pnpm i @arrirpc/codegen-utils@latest)
