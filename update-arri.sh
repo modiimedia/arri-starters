1efd45d4e461ecc17fbb965b0ce9c38afb06edea
Joshua Sosso <josh@modiimedia.com>
2024-05-20 14:55:17 -0500

    update to latest arri

--- update-arri.sh
+++ update-arri.sh
@@ -1,3 +1,3 @@
 (cd app-starter-ts ; echo "\nUpdating \"app-starter-ts\"" ; pnpm i arri@latest @arrirpc/server@latest @arrirpc/schema@latest)
-(cd app-starter-ts-with-eslint ; echo "\nUpdating \"app-starter-ts-with-eslint\"" ; pnpm i arri@latest @arrirpc/server@latest @arrirpc/schema@latest)
+(cd app-starter-ts-with-eslint ; echo "\nUpdating \"app-starter-ts-with-eslint\"" ; pnpm i arri@latest @arrirpc/server@latest @arrirpc/schema@latest ; pnpm i --save-dev @arrirpc/eslint-plugin@latest)
 (cd generator-starter ; echo "\nUpdating \"generator-starter\"" ; pnpm i @arrirpc/codegen-utils@latest)
