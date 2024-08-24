cec44bee03aac9ef137447c6ce4eee0c3ed9e1c8
Joshua Sosso <josh@modiimedia.com>
2024-08-24 12:25:06 -0500

    update arri && update dev dependencies"

---
+++ update.sh
@@ -0,0 +1,3 @@
+(cd app-starter-ts ; echo "\nRunning \"pnpm update\" in \"app-starter-ts\"" ; pnpm update --save)
+(cd app-starter-ts-with-eslint ; echo "\nRunning \"pnpm update\" in \"app-starter-ts-with-eslint\"" ; pnpm update --save)
+(cd generator-starter ; echo "\nRunning \"pnpm update\" in  \"generator-starter\"" ; pnpm update --save)
