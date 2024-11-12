4ae8e026645d42f95032a7bfad4b949cfa0dc56f
Joshua Sosso <josh@modiimedia.com>
2024-11-12 12:28:55 -0600

    modify outdated and update scripts

--- check-outdated.sh
+++ check-outdated.sh
@@ -1,3 +1,4 @@
+(cd app-starter-go ; echo "\nChecking outdated in \"app-starter-go\"" ; pnpm outdated ; go list -u -m all)
 (cd app-starter-ts ; echo "\nChecking outdated in \"app-starter-ts\"" ; pnpm outdated)
 (cd app-starter-ts-with-eslint ; echo "\nChecking outdated in \"app-starter-ts-with-eslint\"" ; pnpm outdated)
 (cd generator-starter ; echo "\nChecking outdated in \"generator-starter\"" ; pnpm outdated)
--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
+(cd app-starter-go ; echo "\nRunning \"pnpm update\" in \"app-starter-go\"" ; pnpm update --save ; echo "\nRunning \"go get -u\" in \"app-starter-go\"" ; go get -u ; echo "\nRunning \"go mod tidy\" in \"app-starter-go\"" ; go mod tidy)
 (cd app-starter-ts ; echo "\nRunning \"pnpm update\" in \"app-starter-ts\"" ; pnpm update --save)
 (cd app-starter-ts-with-eslint ; echo "\nRunning \"pnpm update\" in \"app-starter-ts-with-eslint\"" ; pnpm update --save)
 (cd generator-starter ; echo "\nRunning \"pnpm update\" in  \"generator-starter\"" ; pnpm update --save)
