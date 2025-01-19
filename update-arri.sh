cfd974c54aaecbac45699e683bb32df32ad993e0
Joshua Sosso <josh@modiimedia.com>
2025-01-18 23:07:36 -0600

    update to latest

--- update-arri.sh
+++ update-arri.sh
@@ -1,4 +1,4 @@
-(cd app-starter-go ; echo "\nUpdating \"app-starter-go\"" ; pnpm i arri@latest ; pnpm arri use latest ; go get ; go mod tidy )
-(cd app-starter-ts ; echo "\nUpdating \"app-starter-ts\"" ; pnpm i arri@latest @arrirpc/server@latest @arrirpc/schema@latest)
-(cd app-starter-ts-with-eslint ; echo "\nUpdating \"app-starter-ts-with-eslint\"" ; pnpm i arri@latest @arrirpc/server@latest @arrirpc/schema@latest ; pnpm i --save-dev @arrirpc/eslint-plugin@latest)
+(cd app-starter-go ; echo "\nUpdating \"app-starter-go\"" ; pnpm arri use latest ; pnpm i ; go get ; go mod tidy )
+(cd app-starter-ts ; echo "\nUpdating \"app-starter-ts\"" ; pnpm arri use latest; pnpm i)
+(cd app-starter-ts-with-eslint ; echo "\nUpdating \"app-starter-ts-with-eslint\"" ; pnpm arri use latest; pnpm i;)
 (cd generator-starter ; echo "\nUpdating \"generator-starter\"" ; pnpm i @arrirpc/codegen-utils@latest)
