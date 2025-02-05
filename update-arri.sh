b15c4b3d8fecadbb1ef6fc9831f2a9a00176bd12
joshmossas <josh@modiimedia.com>
2025-02-06 00:01:30 +0800

    update libs

--- update-arri.sh
+++ update-arri.sh
@@ -1,4 +1,4 @@
-(cd app-starter-go ; echo "\nUpdating \"app-starter-go\"" ; pnpm arri use latest ; pnpm i ; go get ; go mod tidy )
+(cd app-starter-go ; echo "\nUpdating \"app-starter-go\"" ; pnpm arri use latest ; pnpm i ; GONOSUMDB=github.com/modiimedia/arri go get ; go mod tidy )
 (cd app-starter-ts ; echo "\nUpdating \"app-starter-ts\"" ; pnpm arri use latest; pnpm i)
 (cd app-starter-ts-with-eslint ; echo "\nUpdating \"app-starter-ts-with-eslint\"" ; pnpm arri use latest; pnpm i;)
 (cd generator-starter ; echo "\nUpdating \"generator-starter\"" ; pnpm i @arrirpc/codegen-utils@latest)
